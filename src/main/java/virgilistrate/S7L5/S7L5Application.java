f698199940b0cab031aece8ea23ba003b30bad3c
virgilistrate <virgilistrate24@gmail.com>
2026-02-20 09:48:27 +0100

    Initial commit

---
+++ src/main/java/virgilistrate/S7L5/S7L5Application.java
@@ -0,0 +1,13 @@
+package virgilistrate.S7L5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S7L5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S7L5Application.class, args);
+	}
+
+}
---
+++ src/test/java/virgilistrate/S7L5/S7L5ApplicationTests.java
@@ -0,0 +1,13 @@
+package virgilistrate.S7L5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S7L5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
